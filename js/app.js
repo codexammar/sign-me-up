12d09f60288ae2a44bd3078fd6fd160bde2e1b3c
AmmarSchool <syed.ammar.ali@torontomu.ca>
2025-04-28 14:31:27 -0400

    Fixed the dark mode stuff to make it like portfolio, now fixing cards and buttons for module 1 and index

--- js/app.js
+++ js/app.js
@@ -1,22 +1,14 @@
 // Dark Mode Toggle
-const toggleButton = document.getElementById('theme-toggle');
-const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const themeToggleBtn = document.getElementById('theme-toggle');
 
-// Load saved theme if any
-if (localStorage.getItem('theme') === 'dark' || (!localStorage.getItem('theme') && prefersDark)) {
-    document.body.classList.add('dark-mode');
-    toggleButton.textContent = '🌞';
-}
+themeToggleBtn.addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark-mode');
 
-toggleButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    if (document.body.classList.contains('dark-mode')) {
-        toggleButton.textContent = '🌞';
-        localStorage.setItem('theme', 'dark');
-    } else {
-        toggleButton.textContent = '🌙';
-        localStorage.setItem('theme', 'light');
-    }
+  if (document.documentElement.classList.contains('dark-mode')) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
 });
 
 // Hamburger Menu Toggle
